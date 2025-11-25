69fb7fdea5071d443b9eec5bcdd5d46b787f0cb7
Jordan Vigneulle <e76320u@etu.univ-lorraine.fr>
2025-11-25 14:45:47 +0100

    Ajout DBConnetion

--- record/src/DBConnection.java
+++ record/src/DBConnection.java
@@ -1,2 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class DBConnection {
+    Properties connectionProps = new Properties();
+    String userName = "root";
+    String password = "";
+    String serverName = "127.0.0.1";
+    String portNumber = "3306";
+    private String tableName = "testpersonne";
+    private static String dbName = "testpersonne";
+    private static Connection connection;
+    private Statement statement;
+    
+    private DBConnection() throws SQLException{
+        Properties prop = new Properties();
+        connectionProps.put("user",userName);
+        connectionProps.put("password", password);
+        String urlDB = "jdbc:mysql://" + serverName + ":";
+        urlDB += portNumber + "/" + dbName;
+        this.connection = DriverManager.getConnection(urlDB, connectionProps);
+    }
+
+    public synchronized Connection getConnection() throws SQLException {
+        if(connection == null){
+            new DBConnection();
+        }
+        return connection;
+    }
+    public static synchronized void setDbName(String nomDB) throws SQLException {
+        if(nomDB == null || nomDB!=dbName){
+            dbName = nomDB;
+            connection = null;
+        }
+    }
 }
